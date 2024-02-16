244c4ec06da6c2acf14afe2c1b6cce0fdf05faef
JesusVillanuev <jesus.villanueva235078@potros.itson.edu.mx>
2024-02-15 22:54:06 -0700

    Modificacion dao cliente

--- bancoNegocio/src/main/java/com/mycompany/banconegocio/operaciones/metodosAuxiliares.java
+++ bancoNegocio/src/main/java/com/mycompany/banconegocio/operaciones/metodosAuxiliares.java
@@ -10,15 +10,5 @@ import java.util.Random;
  */
 public class metodosAuxiliares {
     
-    public int generarNumCuenta(){
-        Random random = new Random();
-        StringBuilder numeroCuenta = new StringBuilder();
-        
-        for (int i = 0; i < 12; i++) {
-            int digito = random.nextInt(10); 
-            numeroCuenta.append(digito);
-        }
-        
-        return Integer.parseInt(numeroCuenta.toString());
-    }
+    
 }
--- bancoPersistencia/src/main/java/com/mycompany/bancopersistencia/DAOS/ClienteDAO.java
+++ bancoPersistencia/src/main/java/com/mycompany/bancopersistencia/DAOS/ClienteDAO.java
@@ -10,6 +10,7 @@ import com.mycompany.bancopersistencia.exception.persistenciaException;
 import com.mycompany.bancopersistencia.Conexion.IConexionBD;
 import com.mysql.cj.jdbc.CallableStatement;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -24,20 +25,88 @@ public class ClienteDAO implements IClienteDAO{
     
     public ClienteDAO(IConexionBD conexion){
         this.conexionBD=conexion;
+        
     }
+    
     
     @Override
     public Cliente registraCliente(ClienteDTO cliente)throws persistenciaException{
-        try(Connection co=this.c) {
+        try(Connection con=this.conexionBD.crearConexion();
+            CallableStatement conn=(CallableStatement)con.prepareCall("{call sp_insertarClienteNuevo(?,?,?,?,?,?,?)}")  ) {
+            
+            conn.setString(1,cliente.getNombres());
+            conn.setString(2,cliente.getApellido_paterno());
+            conn.setString(3,cliente.getApellido_materno());
+            conn.setString(4,cliente.getFechaNacimiento());
+            conn.setString(5,cliente.getUsario());
+            conn.setString(6,cliente.getContraseña());
+            conn.setString(7,cliente.getDomicilio());
+            
+            int registro=conn.executeUpdate();
+            LOG.log(Level.INFO, "Se agregaron con éxito {0} ", registro);
+            
+            ResultSet registroG=conn.getGeneratedKeys();
+            registroG.next();
+            
+            Cliente clienteNuevo=new Cliente(registroG.getInt(1),
+            cliente.getUsario(),
+            cliente.getContraseña(), 
+            cliente.getFechaNacimiento(), 
+            cliente.getNombres(), 
+            cliente.getApellido_paterno(), 
+            cliente.getApellido_materno(),
+            cliente.getDomicilio());
+            
+            return clienteNuevo;
            
         } catch (Exception e) {
+            LOG.log(Level.SEVERE, "No se ha podido agregar el cliente",e);
+            throw new persistenciaException("No se pudo guardar", e);
         }
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        
     }
 
     @Override
     public Cliente editarCliente(ClienteDTO cliente)throws persistenciaException{
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        try(Connection con=this.conexionBD.crearConexion();
+            CallableStatement conn=(CallableStatement)con.prepareCall("{call sp_actualizar_cliente(?,?,?,?,?,?,?,?)}")  ) {
+            
+            conn.setInt(1, cliente.getIdCliente());
+            conn.setString(2,cliente.getNombres());
+            conn.setString(3,cliente.getApellido_paterno());
+            conn.setString(4,cliente.getApellido_materno());
+            conn.setString(5,cliente.getFechaNacimiento());
+            conn.setString(6,cliente.getUsario());
+            conn.setString(7,cliente.getContraseña());
+            conn.setString(8,cliente.getDomicilio());
+            
+            int registro=conn.executeUpdate();
+            LOG.log(Level.INFO, "Se actualizo con éxito {0} ", registro);
+            
+            if (registro>0) {
+                ResultSet registroG=conn.getGeneratedKeys();
+                registroG.next();
+                
+                Cliente clienteNuevo=new Cliente(registroG.getInt(1),
+                cliente.getUsario(),
+                cliente.getContraseña(), 
+                cliente.getFechaNacimiento(), 
+                cliente.getNombres(), 
+                cliente.getApellido_paterno(), 
+                cliente.getApellido_materno(),
+                cliente.getDomicilio());
+                
+                return clienteNuevo;
+            }else{
+                throw new persistenciaException("No se pudo actualizar el cliente. No se encontraron registros afectados");
+            }
+            
+            
+        } catch (Exception e) {
+            LOG.log(Level.SEVERE, "Error al actualizar el cliente");
+            throw new persistenciaException("Fallo al actualizar el cliente",e);
+        }
+        
     }
     
 }
